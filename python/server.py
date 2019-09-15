95ab104964b7e66e6bb2f103b6d8c4c27ba5d11c
Indraneel Srivastava <indraneel.max@gmail.com>
2019-09-15 14:24:15 +0100

    Initial first working version flask socketio

---
+++ python/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__, static_url_path = "")
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@socketio.on('my event')
+def handle_message(message):
+    print('received on server my event: ', message)
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=5000)
+    print "Runnning...."
